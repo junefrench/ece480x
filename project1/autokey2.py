b733b886da412d06145bdbc118d53cfdc67ce39e
John French <jdaviesf7@gmail.com>
2015-03-18 11:14:06 -0400

    Added cyphertext-autokey implementation and script to break it

--- project1/autokey2.py
+++ project1/autokey2.py
@@ -29,33 +29,33 @@ from string import ascii_uppercase
 import itertools
 
 """
-Implementation of Vigenere's Plaintext-Autokey Cypher
+Implementation of Vigenere's Cyphertext-Autokey Cypher
 """
 
-def gen(keyword, plaintext, tokens=ascii_uppercase):
+def gen(keyword, cyphertext, tokens=ascii_uppercase):
     def check(token):
         if token not in tokens:
             raise ValueError()
         return token
-    return map(check, itertools.chain(keyword, plaintext))
+    return map(check, itertools.chain(keyword, cyphertext))
 
 def enc(keyword, plaintext, tokens=ascii_uppercase):
-    key = gen(keyword, plaintext, tokens=tokens)
+    cyphertext = []
+    key = gen(keyword, cyphertext, tokens=tokens)
     def enc_token(key_token, plain_token):
         if plain_token not in tokens:
             raise ValueError()
-        return tokens[(tokens.index(key_token) + tokens.index(plain_token)) % len(tokens)]
+        cypher_token = tokens[(tokens.index(key_token) + tokens.index(plain_token)) % len(tokens)]
+        cyphertext.append(cypher_token) # for the key generator
+        return cypher_token
     return map(enc_token, key, plaintext)
 
 def dec(keyword, cyphertext, tokens=ascii_uppercase):
-    plaintext = []
-    key = gen(keyword, plaintext, tokens=tokens)
+    key = gen(keyword, cyphertext, tokens=tokens)
     def dec_token(key_token, cypher_token, tokens=tokens):
         if cypher_token not in tokens:
             raise ValueError()
-        plain_token = tokens[(tokens.index(cypher_token) - tokens.index(key_token)) % len(tokens)]
-        plaintext.append(plain_token) # for the key generator
-        return plain_token
+        return tokens[(tokens.index(cypher_token) - tokens.index(key_token)) % len(tokens)]
     return map(dec_token, key, cyphertext)
 
 if __name__ == '__main__':
