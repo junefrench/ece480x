ebbd758293420ea97d0118cd1befa6a4b62c3b31
John French <jdaviesf7@gmail.com>
2015-03-25 14:50:24 -0400

    Finished project 2 question 1

---
+++ project2/lfsr.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+
+# This is free and unencumbered software released into the public domain.
+# 
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+# 
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# 
+# For more information, please refer to <http://unlicense.org/>
+
+"""
+Implementation of the LFSR cypher from book problem 2.11
+"""
+
+def gen(token_bits = 5, degree=6, initial_vector=0b111111, coefficients=0b0000110):
+    """
+    LFSR keystream generator
+
+    Parameters:
+        degree: the number of terms in the LFSR
+        initial_vector: an integer, the bits of which comprise the initial values of each term in the LFSR (the LSB will be the first bit yielded)
+        coefficients: an integer, the bits of which comprise the coefficients of each term (where the LSB is the constant term)
+
+    Returns: a generator which yields ints corresponding to the tokens of the keystream
+    """
+    from gmpy import popcount
+    x = initial_vector
+    out_byte = 0
+    out_ind = 0
+    while True:
+        out_byte |= (x & 1) << (token_bits - 1 - out_ind)
+        out_ind = (out_ind + 1) % token_bits
+        if out_ind == 0:
+            yield out_byte
+            out_byte = 0
+        feedback = popcount(((x << 1) | 1) & coefficients) % 2
+        x = (x >> 1) | (feedback << (degree - 1))
+
+def enc(p, k):
+    for pc, kc in zip(p, k):
+        yield pc ^ kc
+
+def dec(c, k):
+    return enc(c, k)  # decryption is the same algorithm as encryption, just xor
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(description="Encrypt or decrypt messages with an LFSR")
+    parser.add_argument('command', choices=['encrypt', 'decrypt'], help='The operation to perform')
+    parser.add_argument('text', help='The plaintext to encrypt or cyphertext to decrypt')
+    args = parser.parse_args()
+
+    def encode(t):
+        from string import ascii_uppercase
+        for c in t:
+            if c in ascii_uppercase:
+                yield ord(c) - ord('A')
+            elif c in '012345':
+                yield ord(c) - ord('0') + 26
+            else:
+                raise ValueError()
+
+    def decode(e):
+        for c in e:
+            if c < 26:
+                yield chr(ord('A') + c)
+            elif c < 32:
+                yield chr(ord('0') + c - 26)
+
+    if args.command == 'encrypt':
+        print(''.join(decode(enc(encode(args.text.upper()), gen()))))
+    elif args.command == 'decrypt':
+        print(''.join(decode(dec(encode(args.text.upper()), gen()))).lower())
+
--- project2/writeup.md
+++ project2/writeup.md
@@ -23,11 +23,11 @@ John French
 >>
 >> We observe now on the channel the following message: `j5a0edj2b`
 
-## a
+### a
 
 >> What is the initialization vector?
 
-We can determine the beginning of the keystring from the header `WPI` and the cyphertext:
+We can determine the beginning of the keystream by XORing the known partial plaintext `wpi` with the cyphertext:
 
 ```
 Plaintext:  {-w-}{-p-}{-i-}{-?-}{-?-}{-?-}{-?-}{-?-}{-?-}
@@ -40,19 +40,50 @@ Keystream:  {-5-}{-Q-}{-I-}{-?-}{-?-}{-?-}{-?-}{-?-}{-?-}
 
 The initialization vector is the first `m` bytes of the keystream, so `111111`.
 
-## b
+### b
 
 >> What are the feedback coefficients of the LFSR?
 
-## c
+The feedback to the LFSR for each clock tick is:
+```
+t = 0: 111111 -> 0
+t = 1: 011111 -> 0
+t = 2: 001111 -> 0
+t = 3: 000111 -> 0
+t = 4: 000011 -> 0
+t = 5: 000001 -> 1
+t = 6: 100000 -> 0
+t = 7: 010000 -> 0
+t = 8: 001000 -> ?
+```
+
+One bit changes in the input each tick from `t = 1` to `t = 4`.
+The bit which changes for each tick `t` is the bit `x[t - 1]`.
+Because the feedback bit does not change for any of these steps we know that `x[0]` through `x[3]` all have coefficients of 0.
+On `t = 5` the bit `x[4]` changes, as does the feedback, so this bit has a coefficient of 1.
+On `t = 6` the bits `x[0]` and `x[5]` both change, as does the feedback.
+We already know that `x[0]` has a coefficient of 0 so `x[5]` must have a coefficient of 1.
+On `t = 7` the bits `x[0]` and `x[1]` change, and the feedback does not, as expected (since we know they both have coefficients of 0).
+
+Now we know the coefficients of each bit, but we still need the constant.
+When all bits are 1, the output is 0 (at `t = 0`).
+There are two bits with coefficients of 1, so the feedback will be 0 if the constant term is 0.
+The polynomial is therefore `0&x[0] ^ 0&x[1] ^ 0&x[2] ^ 0&x[3] ^ 1&x[4] ^ 1&x[5] ^ 0`.
+
+### c
 
 >> Write a program in your favorite programming language which generates the whole sequence, and find the whole plaintext.
 
-## d
+The program can be found in `lfsr.py`. The plaintext is `wpiwombat`.
+
+### d
 
 >> Where does the thing after `WPI` live?
 
-## e
+Australia
+
+### e
 
 >> What type of attack did we perform?
 
+A known-plaintext attack
